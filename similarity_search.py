a162904df6f911aa153a887f7e2786049f5caf06
shollercoaster <saumyachat03@gmail.com>
2024-09-04 14:32:07 +0200

    working code for mpnet

--- similarity_search.py
+++ similarity_search.py
@@ -16,11 +16,11 @@ def load_embeddings(embeddings_df_save_path: str) -> tuple:
     text_chunks_and_embedding_df = pd.read_csv(embeddings_df_save_path)
     device = "cuda"
     # Convert embedding column back to np.array (it got converted to string when it got saved to CSV)
-    text_chunks_and_embedding_df["embedding"] = text_chunks_and_embedding_df["embedding"].apply(lambda x: np.fromstring(x.strip("[]"), sep=" "))
+    text_chunks_and_embedding_df["embeddings"] = text_chunks_and_embedding_df["embeddings"].apply(lambda x: np.fromstring(x.strip("[]"), sep=" "))
     # Convert texts and embedding df to list of dicts
     pages_and_chunks = text_chunks_and_embedding_df.to_dict(orient="records")
     # Convert embeddings to torch tensor and send to device (note: NumPy arrays are float64, torch tensors are float32 by default)
-    embeddings = torch.tensor(np.array(text_chunks_and_embedding_df["embedding"].tolist()), dtype=torch.float32).to(device)
+    embeddings = torch.tensor(np.array(text_chunks_and_embedding_df["embeddings"].tolist()), dtype=torch.float32).to(device)
     
     return embeddings, pages_and_chunks
 
@@ -37,23 +37,21 @@ def retrieve_relevant_resources(query: str,
     """
 
     # Embed the query
-    query_prompt_name = "s2p_query"
     query_embedding = model.encode(query,
-                                   prompt_name=query_prompt_name,
                                    convert_to_tensor=True,
                                    device="cuda")
 
     # Get dot product or cosine_similarity scores on embeddings
     start_time = time.time()
 
-    # cosine_similarity_scores = cosine_similarity(query_embedding, embeddings)
-    dot_scores = util.dot_score(query_embedding, embeddings)[0]
+    cosine_similarity_scores = torch.nn.functional.cosine_similarity(query_embedding, embeddings)
+#    dot_scores = util.dot_score(query_embedding, embeddings)[0]
     end_time = time.time()
     
     if print_time:
         print(f"[INFO] Time taken to get scores on {len(embeddings)} embeddings: {end_time-start_time:.5f} seconds.")
     
-    scores, indices = torch.topk(input=dot_scores,
+    scores, indices = torch.topk(input=cosine_similarity_scores,
                                  k=n_resources_to_return)
     
     return scores, indices
