131c210e62d51453e75f708e5d3df5651c06861a
shollercoaster <saumyachat03@gmail.com>
2024-09-06 10:00:41 +0200

    common file for creating multiple text embeddings

---
+++ generate_text_embeddings.py
@@ -0,0 +1,21 @@
+from cumulative_pdf_processing import process_pdf_for_embeddings
+
+# Embeddings for OSTEP
+process_pdf_for_embeddings(
+    pdf_path="data/operating_systems_three_easy_pieces.pdf", 
+    start_page=38, 
+    end_page=623, 
+    num_sentence_chunk_size=8, 
+    min_token_length=30, 
+    output_file="ostep_text_chunks_and_embeddings.csv"
+)
+
+# Embeddings for Neural Network Foundations
+process_pdf_for_embeddings(
+    pdf_path="data/neural-network-learning-theoretical-foundations.pdf", 
+    start_page=15, 
+    end_page=370,
+    num_sentence_chunk_size=4, 
+    min_token_length=30, 
+    output_file="neural_network_text_chunks_and_embeddings.csv"
+)
